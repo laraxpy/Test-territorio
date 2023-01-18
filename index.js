f60d72a4f604f4e04197a6e6199f921196c47434
Juan Marcelo Maldonado <laraxasd@gmail.com>
2023-01-18 17:31:40 -0300

    agregando certificado SSL

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const https = require('https')
 const router = require('./router/index')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 const {engine} = require('express-handlebars');
 const path = require('path');
+https.createServer({
+    cert: 'mi_certificado.crt',
+    key: 'mi_certificado.key'
+}, app).listen(port, ()=> {
+    console.log(`Servidor escuchando en puerto ${port}`)
+})
+
 //Habilitamos handlebars como view
 app.engine('handlebars', engine({defaultLayout: 'layout'}));
 app.set('view engine','handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router());
 
-app.listen(port, ()=> {
-    console.log('Servidor escuchando en puerto 3000')
-})

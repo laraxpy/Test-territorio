ecb8dc67a0d776dde8b6758ac7cf0afc7b2b0223
Juan Marcelo Maldonado <laraxasd@gmail.com>
2023-01-18 22:00:19 -0300

    Agregando redireccion

--- router/index.js
+++ router/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controller/homeController');
 
 module.exports= ()=>{
+    router.get('*', function(req,res){res.redirect('https://' + req.headers.hot + req.url)});
     router.get('/', homeController.pagInicio)
     router.get('/miterritorio', homeController.miTerritorio)
     router.get('/mapa', homeController.mapa)

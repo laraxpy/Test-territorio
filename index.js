dc7a7f65a3e6effeb1c1a95b016e7e2a4d5d244e
Juan Marcelo Maldonado <laraxasd@gmail.com>
2023-01-18 21:44:09 -0300

    Ultima actualizacion

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const https = require('https')
+const https = require('https');
+const fs = require('fs');
 const router = require('./router/index')
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 3000;
 const {engine} = require('express-handlebars');
 const path = require('path');
 https.createServer({
-    cert: 'mi_certificado.crt',
-    key: 'mi_certificado.key'
-}, app).listen(port, ()=> {
-    console.log(`Servidor escuchando en puerto ${port}`)
-})
+    key: fs.readFileSync('/etc/letsencrypt/live/territoriofernando.ddns.net/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/territoriofernando.ddns.net/fullchain.pem')
+}, app).listen(port, ()=> {})
 
 //Habilitamos handlebars como view
 app.engine('handlebars', engine({defaultLayout: 'layout'}));
 app.set('view engine','handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router());
-
